3cea0d76783aacc736311092a21268941cf1be03
Giusy Mancuso <giusy219@gmail.com>
2022-04-15 10:02:54 +0200

    added login activity

--- app/src/main/java/com/android/example/cartoleria/LoginFragment.kt
+++ app/src/main/java/com/android/example/cartoleria/LoginFragment.kt
@@ -5,15 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
-import com.android.example.cartoleria.databinding.FragmentFirstBinding
+import com.android.example.cartoleria.databinding.FragmentLoginBinding
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class First2Fragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentLoginBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +26,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,11 +34,12 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener{
-            findNavController().navigate(R.id.action_FirstFragment_to_mainActivity)
-        }
-        binding.buttonFirst.setOnClickListener{
-            findNavController().navigate(R.id.action_FirstFragment_to_first2Fragment)
+        binding.login.setOnClickListener{
+            if (binding.inputEmail.text.contains("@")){
+                findNavController().navigate(R.id.action_first2Fragment_to_mainActivity)
+            }else {
+                Snackbar.make(binding.login, R.string.Error, Snackbar.LENGTH_SHORT).show()
+            }
         }
     }
 
